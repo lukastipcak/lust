eac8f009cdf6d5c3e6691e686dad0853167e7979
Lukáš Štipčák <lukasstipcak@Lukas--MacBook-Air.local>
2026-02-10 14:51:29 +0100

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/lukastipcak/lust',   // DŮLEŽITÉ
+  assetPrefix: '/lukastipcak/lust/',
 };
 
 export default nextConfig;
